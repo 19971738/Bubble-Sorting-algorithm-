225f02111b0d35d2aa2e651faa88d16c7dac3034
Ponce Streaming <46233347+19971738@users.noreply.github.com>
2020-04-30 17:45:56 -0500

    Add files via upload

---
+++ Sorting manually 2.py
@@ -0,0 +1,15 @@
+def bubblesort(list):
+
+# It requieres 3 steps at least, two are for
+# the porpose of checking and the 3rd for making sure everything is all right
+    for iter_num in range(len(list)-1,0,-1):
+        for idx in range(iter_num):
+            if list[idx]>list[idx+1]:
+                temp = list[idx]
+                list[idx] = list[idx+1]
+                list[idx+1] = temp
+
+
+list = [19,2,31,45,6,11,121,27]
+bubblesort(list)
+print(list)
